9a68785a1e568926f66b7c1bfaf197fc76db59f4
vlach1989 <vlach1989@gmail.com>
2020-05-29 09:12:59 +0200

    MapZoomBoxRangeRange updates

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,7 @@ module.exports = function override(config, env) {
 			// '@gisatcz/cross-package-react-context': "C:/Users/pvlach/DATA/cross-package-react-context",
 			// "@gisatcz/ptr-core": "C:/Users/pvlach/DATA/ptr-core",
 			// "@gisatcz/ptr-charts": "C:/Users/pvlach/DATA/ptr-charts",
-			// "@gisatcz/ptr-maps": "C:/Users/pvlach/DATA/ptr-maps",
+			"@gisatcz/ptr-maps": "C:/Users/pvlach/DATA/ptr-maps",
 			// "@gisatcz/ptr-utils": "C:/Users/pvlach/DATA/ptr-utils",
 			// "@gisatcz/ptr-core": "C:/Users/pvlach/DATA/ptr-core",
 			// "@gisatcz/ptr-state": "C:/Users/pvlach/DATA/ptr-state",
--- src/components/MapZoomBoxRangeRange.js
+++ src/components/MapZoomBoxRangeRange.js
@@ -1,30 +1,49 @@
 import React from 'react';
-import {PresentationMap, MapControls, WorldWindMap} from "@gisatcz/ptr-maps";
+import {PresentationMap, MapControls, WorldWindMap, ReactLeafletMap, MapSet, MapSetMap, MapSetPresentationMap} from "@gisatcz/ptr-maps";
 
 const view = {
     center: {lat: 50, lon: 15},
-    boxRange: 2000000
+    boxRange: 1000000
 };
 
-const presentational_backgroundLayer = {
+const viewLimits = {
+  boxRangeRange: [50000,2000000]
+};
+
+const osm = {
     key: 'background-osm',
     type: 'wmts',
     options: {
-        url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     }
 };
 
 class MapZoomBoxRangeRange extends React.PureComponent {
     render() {
         return (
-            <div className="ptr-light" style={{height: 600}}>
-                <PresentationMap
-                    mapComponent={WorldWindMap}
-                    backgroundLayer={presentational_backgroundLayer}
-                    view={view}
-                >
-                    <MapControls/>
-                </PresentationMap>
+            <div className="ptr-light" style={{display: 'flex', flexWrap: 'wrap'}}>
+                <div style={{height: 400, width: 500, margin: 10}}>
+                    <PresentationMap
+                        mapComponent={ReactLeafletMap}
+                        backgroundLayer={osm}
+                        view={view}
+                        viewLimits={viewLimits}
+                        levelsBased={true}
+                    >
+                        <MapControls zoomOnly levelsBased/>
+                    </PresentationMap>
+                </div>
+                <div style={{height: 400, width: 500, margin: 10}}>
+                    <PresentationMap
+                        mapComponent={WorldWindMap}
+                        backgroundLayer={osm}
+                        view={view}
+                        viewLimits={viewLimits}
+                        levelsBased={true}
+                    >
+                        <MapControls zoomOnly levelsBased/>
+                    </PresentationMap>
+                </div>
             </div>
         );
     }
