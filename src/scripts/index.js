ddebbbe4c4b80d6dc115e859ef3712710ef0f7ad
vlach1989 <vlach1989@gmail.com>
2020-06-24 14:21:11 +0200

    jsonAdjustment script update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,32 +2,62 @@ import {adjustGeoJson} from "./jsonAdjustment";
 
 import riskZones from "./jsonAdjustment/riskZones.json";
 import road from "./jsonAdjustment/road.json";
+import t41 from "./jsonAdjustment/t41_orig.json";
 
 function run() {
-    adjustGeoJson(road, 'road', {
-        properties: {
-            update: {
-                "km": {decimals: 2, nullValue: 9999},
-                "max": {decimals: 2, nullValue: 9999},
-                "mean": {decimals: 2, nullValue: 9999},
-                "median": {decimals: 2, nullValue: 9999},
-                "min": {decimals: 2, nullValue: 9999},
-                "p25": {decimals: 2, nullValue: 9999},
-                "p75": {decimals: 2, nullValue: 9999},
-                "p75_weight": {decimals: 2, nullValue: 9999},
-                "A": {nullValue: 9999},
-                "B": {nullValue: 9999},
-                "C": {nullValue: 9999},
-                "D": {nullValue: 9999},
-                "haz_0_10": {nullValue: 9999},
-                "haz_10_20": {nullValue: 9999},
-                "haz_20_30": {nullValue: 9999},
-                "haz_30_40": {nullValue: 9999},
-                "haz_40_50": {nullValue: 9999},
-                "haz_50_": {nullValue: 9999}
-            }
-        }
-    });
+    // adjustGeoJson(t41, 't41', {
+    //     properties: {
+    //         remove: {
+    //             regexps: [/^m_/],
+    //             list: ['lat', 'lon', 'h_sd', 'vel_sd', 'ktemp', 'svet', 'lvet', 'in', 'fin', 'st_dev', 'cl_jmp', 's0_mod', 'vacc_mod', 'cl_noise', 'vel_last', 'cl_ue', 'corr_ue', "ws_zone","aoi01_class_rr","size","vel_last","td_last","svel_last","std_last", "href"]
+    //         },
+    //         updateRegexp: {
+    //             regexp: /^d_/,
+    //             integer: true,
+    //             nullValue: 9999
+    //         },
+    //         update: {
+    //             "h": {decimals: 2, nullValue: 9999},
+    //             "h_ref": {decimals: 2, nullValue: 9999},
+    //             "vel_avg": {decimals: 2, nullValue: 9999},
+    //             "vel_cum": {decimals: 2, nullValue: 9999},
+    //             "coh": {decimals: 2, nullValue: 9999},
+    //             "s0": {decimals: 2, nullValue: 9999},
+    //             "inc_ang": {decimals: 2, nullValue: 9999},
+    //             "stdev": {decimals: 2, nullValue: 9999},
+    //             "coh_mod": {decimals: 2, nullValue: 9999},
+    //             "sens": {decimals: 2, nullValue: 9999},
+    //             "aspect": {decimals: 2, nullValue: 9999},
+    //             "slope": {decimals: 2, nullValue: 9999},
+    //             "az_ang": {decimals: 2, nullValue: 9999},
+    //         }
+    //     }
+    // });
+
+    // adjustGeoJson(road, 'road', {
+    //     properties: {
+    //         update: {
+    //             "km": {decimals: 2, nullValue: 9999},
+    //             "max": {decimals: 2, nullValue: 9999},
+    //             "mean": {decimals: 2, nullValue: 9999},
+    //             "median": {decimals: 2, nullValue: 9999},
+    //             "min": {decimals: 2, nullValue: 9999},
+    //             "p25": {decimals: 2, nullValue: 9999},
+    //             "p75": {decimals: 2, nullValue: 9999},
+    //             "p75_weight": {decimals: 2, nullValue: 9999},
+    //             "A": {nullValue: 9999},
+    //             "B": {nullValue: 9999},
+    //             "C": {nullValue: 9999},
+    //             "D": {nullValue: 9999},
+    //             "haz_0_10": {nullValue: 9999},
+    //             "haz_10_20": {nullValue: 9999},
+    //             "haz_20_30": {nullValue: 9999},
+    //             "haz_30_40": {nullValue: 9999},
+    //             "haz_40_50": {nullValue: 9999},
+    //             "haz_50_": {nullValue: 9999}
+    //         }
+    //     }
+    // });
 }
 
 export default run();
--- src/scripts/jsonAdjustment/index.js
+++ src/scripts/jsonAdjustment/index.js
@@ -29,9 +29,56 @@ import _ from "lodash";
  * options.properties.remove example:
  * {
  *     list: [],
- *     regexp: []
+ *     regexps: []
  * }
  */
+
+export function adjustGeoJson(json, outputName, options) {
+    json.features.forEach(feature => {
+        if (options?.properties) {
+            if (options.properties.remove) {
+                feature.properties = removeProperties(feature.properties, options.properties.remove);
+            }
+
+            if (options.properties.updateRegexp) {
+                feature.properties = updatePropertiesRegexp(feature.properties, options.properties.updateRegexp);
+            }
+
+            if (options.properties.update) {
+                feature.properties = updateProperties(feature.properties, options.properties.update);
+            }
+        }
+
+        return feature;
+    });
+
+    download(JSON.stringify(json), outputName + ".json", "text/plain");
+}
+
+function updatePropertiesRegexp(properties, update) {
+    let fitProperties = _.pickBy(properties, (value, key) => update.regexp.test(key));
+    let restProperties = _.omitBy(properties, (value, key) => update.regexp.test(key));
+
+    let newProperties = {};
+    _.forIn(fitProperties, (value, key) => {
+        const operations = update;
+        if (operations) {
+            if (operations.nullValue && operations.nullValue === value) {
+                newProperties[key] = null;
+            } else if (operations.integer) {
+                newProperties[key] = Math.round(value);
+            } else if (operations.decimals) {
+                newProperties[key] = Math.round((value + Number.EPSILON) * Math.pow(10, operations.decimals)) / Math.pow(10, operations.decimals);
+            } else {
+                newProperties[key] = value;
+            }
+        } else {
+            newProperties[key] = value;
+        }
+    });
+
+    return {...restProperties, ...newProperties};
+}
 
 function updateProperties(properties, update) {
     let newProperties = {};
@@ -40,6 +87,8 @@ function updateProperties(properties, update) {
         if (operations) {
             if (operations.nullValue && operations.nullValue === value) {
                 newProperties[key] = null;
+            } else if (operations.integer) {
+                newProperties[key] = Math.round(value);
             } else if (operations.decimals) {
                 newProperties[key] = Math.round((value + Number.EPSILON) * Math.pow(10, operations.decimals)) / Math.pow(10, operations.decimals);
             } else {
@@ -53,14 +102,18 @@ function updateProperties(properties, update) {
     return newProperties;
 }
 
-export function adjustGeoJson(json, outputName, options) {
-    json.features.forEach(feature => {
-        if (options?.properties?.update) {
-            feature.properties = updateProperties(feature.properties, options.properties.update);
-        }
+function removeProperties(properties, remove) {
+    let newProperties = {...properties};
 
-        return feature;
-    });
+    if (remove.regexps) {
+        remove.regexps.forEach(regexp => {
+            newProperties = _.omitBy(newProperties, (value, key) => regexp.test(key));
+        });
+    }
 
-    download(JSON.stringify(json), outputName + ".json", "text/plain");
+    if (remove.list) {
+        newProperties = _.omitBy(newProperties, (value, key) => _.includes(remove.list, key));
+    }
+
+    return newProperties;
 }
