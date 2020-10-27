6eb4435119fbfa0fbb8ec9fb0e91929fbdd0b1f7
vlach1989 <vlach1989@gmail.com>
2020-10-27 12:11:13 +0100

    Updates

---
+++ precache-manifest.04642ac0401d14971c9e1229748462f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7e0e1beff28935c41629c6ab799b98",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "d74f23ff29f6959a0abc",
+    "url": "/playground/static/css/2.8542bfbd.chunk.css"
+  },
+  {
+    "revision": "7c4ba78705bab32a8c4f",
+    "url": "/playground/static/css/main.4709d1b1.chunk.css"
+  },
+  {
+    "revision": "d74f23ff29f6959a0abc",
+    "url": "/playground/static/js/2.6042b60e.chunk.js"
+  },
+  {
+    "revision": "d907c2dc256222e7c7bd11d1ce7a0fcf",
+    "url": "/playground/static/js/2.6042b60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4ba78705bab32a8c4f",
+    "url": "/playground/static/js/main.eda7ded5.chunk.js"
+  },
+  {
+    "revision": "895c97baf350dc475b41",
+    "url": "/playground/static/js/runtime-main.927a30d0.js"
+  }
+]);
