bd300d50f4422990587e18052bea05b4b5f2c426
vlach1989 <vlach1989@gmail.com>
2020-12-19 06:19:30 +0100

    updatedLeaflet

--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -4,12 +4,28 @@ import {Button} from "@gisatcz/ptr-atoms";
 import {PresentationMap, MapControls, ReactLeafletMap} from "@gisatcz/ptr-maps";
 
 import france from "../data/fra_gadm_5_points.json";
+import cz from "../data/czGadm.json";
+import france_poly from "../data/fra_gadm_5_original.json";
+import roads from "../data/roads.json";
 // import france from "../data/france_gadm_4.json";
 
 const view = {
 	center: {lat: 46, lon: 5},
 	boxRange: 500000
 };
+
+// const view = {
+// 	center: {lat: 50, lon: 15},
+// 	boxRange: 500000
+// };
+
+// const view = {
+// 	boxRange: 5000,
+// 	center: {
+// 		lat: 10.2596512,
+// 		lon: 105.946754
+// 	}
+// }
 
 const osm = {
 	key: 'background-osm',
@@ -103,7 +119,7 @@ const styleAsPoints = {
 		styles: [{
 			outlineWidth: 0,
 			outlineColor: "#000000",
-			fillOpacity: 0.7,
+			fillOpacity: 1,
 			outlineOpacity: 1,
 			size: 1200
 		}, {
@@ -155,6 +171,160 @@ const styleAsPoints = {
 }
 
 
+const styleAsPolygonsCz = {
+	rules: [{
+		styles: [{
+			outlineWidth: 2,
+			outlineColor: "#888888",
+			fillOpacity: 0.5,
+			fill: "#888888",
+			outlineOpacity: 1
+		}, {
+			attributeKey: "attr-2",
+			attributeValues: {
+				"red": {
+					fill: "#ff0000",
+				},
+				"green": {
+					fill: "#00ff00"
+				},
+				"blue": {
+					fill: "#0000ff"
+				}
+			}
+		}]
+	}]
+}
+
+const styleAsPolygonsFra = {
+	rules: [{
+		styles: [{
+			outlineWidth: 1,
+			outlineColor: "#888888",
+			// fillOpacity: 0.5,
+			fill: "#dddddd",
+			outlineOpacity: .5,
+			fillOpacity: .5
+		}, {
+			attributeKey: "GID_1",
+			attributeValues: {
+				"FRA.3_1": {
+					fill: "#0000ff"
+				},
+				"FRA.4_1": {
+					fill: "#00ffff"
+				},
+				"FRA.5_1": {
+					fill: "#ff00ff"
+				},
+				"FRA.6_1": {
+					fill: "#ffff00"
+				},
+				"FRA.7_1": {
+					fill: "#aaffcc"
+				},
+				"FRA.8_1": {
+					fill: "#cf18cc"
+				},
+				"FRA.9_1": {
+					fill: "#8118cc"
+				},
+				"FRA.10_1": {
+					fill: "#8aa8cc"
+				},
+				"FRA.11_1": {
+					fill: "#8a7818"
+				},
+				"FRA.12_1": {
+					fill: "#8aa818"
+				},
+				"FRA.13_1": {
+					fill: "#111818"
+				}
+			}
+		}]
+	}]
+}
+
+const lineStyle = {
+	rules: [{
+		styles: [{
+			outlineColor: "#555555",
+			outlineWidth: 6,
+			outlineOpacity: 1
+		}, {
+			attributeKey: "vel_avg",
+			attributeClasses: [
+				{
+					interval: [-1000, -50],
+					name: "< -50",
+					outlineColor: "#b1001d",
+					"intervalBounds": [
+						false,
+						true
+					],
+				}, {
+					interval: [-50, -40],
+					outlineColor: "#c8292d",
+					"intervalBounds": [
+						false,
+						true
+					],
+				}, {
+					interval: [-40, -30],
+					outlineColor: "#de533d",
+					"intervalBounds": [
+						false,
+						true
+					],
+				}, {
+					interval: [-30, -20],
+					outlineColor: "#f47d4e",
+					"intervalBounds": [
+						false,
+						true
+					],
+				}, {
+					interval: [-20, -10],
+					"intervalBounds": [
+						false,
+						true
+					],
+					outlineColor: "#f9d817"
+				}, {
+					interval: [-10, 0],
+					outlineColor: "#a0f000",
+					"intervalBounds": [
+						false,
+						true
+					]
+				}, {
+					interval: [0, 10],
+					"intervalBounds": [
+						false,
+						true
+					],
+					outlineColor: "#50d70d"
+				}, {
+					interval: [10, 20],
+					"intervalBounds": [
+						false,
+						true
+					],
+					outlineColor: "#92c5de"
+				}, {
+					interval: [20, 1000],
+					"intervalBounds": [
+						false,
+						false
+					],
+					name: "> 20",
+					outlineColor: "#7198da"
+				}],
+		}]
+	}]
+}
+
 const layers = [
 	{
 	key: "layer",
@@ -180,7 +350,7 @@ const layers = [
 		// }],
 		pointAsMarker: false,
 		// tiles: getFirstTiledFeatures(france.features, 500),
-		features: france.features,
+		features: france_poly.features,
 		hoverable: true,
 		hovered: {
 			style: "default"
@@ -192,10 +362,31 @@ const layers = [
 				hoveredStyle: "default"
 			}
 		},
-		style: styleAsPoints,
+		style: styleAsPolygonsFra,
 		fidColumnName: "GID_5"
 	}
 }];
+
+// const layers = [{
+// 	key: "risk-assessment-roads-big",
+// 	name: "Roads",
+// 	type: "canvas-vector",
+// 	options: {
+// 		features: roads.features,
+// 		style: lineStyle,
+// 		fidColumnName: "gid",
+// 		selectable: true,
+// 		hoverable: true,
+// 		selected: {
+// 			riskAssessmentRoads: {}
+// 		},
+// 		hovered: {
+// 			style: {
+// 				outlineColor: "#00ffff"
+// 			}
+// 		}
+// 	}
+// }];
 
 const style = {
 	rules: [{
